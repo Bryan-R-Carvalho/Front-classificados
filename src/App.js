d77a28c01b1b848cbc08392f0cf46d1fca22290a
Bryan-R-Carvalho <reisdecarvalhobryan@gmail.com>
2021-09-30 14:36:03 -0300

    configuracao de ambiente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Categoria from './categoria/Categoria';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+      <div className="container-fluid">
+
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/categorias">
+            <Categoria/>
+          </Route>
+        </Switch>
+      </div>
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
