aa07a9503d3ac7df479eabc12134d5f70439bbde
Bryan-R-Carvalho <reisdecarvalhobryan@gmail.com>
2021-10-08 10:15:47 -0300

    POST funcionando

--- src/categoria/Categoria.js
+++ src/categoria/Categoria.js
@@ -4,20 +4,30 @@ import './styles.css';
 export default class Categoria extends Component {
     state = {
         id: "",
-        nome: ""
+        nome: "",
+        categorias:[]
     }
 
     txtId_change = (event) =>{
         this.setState({ id: event.target.value})
     }
     txtNome_change = (event) =>{
-        this.setState({ nome: event.target.value})
+        this.setState({nome: event.target.value})
+    }
+    preencherLista = () => {
+        const url = window.servidor + '/categorias/'
+        fetch(url)
+            .then(response => response.json())
+            .then(data => this.setState({categorias: data}))
+    }
+
+    componentDidMount(){
+        this.preencherLista()
     }
     
-    gravarNovo = () =>{
+    gravarNovo = () => {
         const dados = {
-            "id": this.state.id,
-            "nome": this.state.nome
+            "nome": this.state.nome 
         }
 
         const requestOptions = {
@@ -28,7 +38,7 @@ export default class Categoria extends Component {
         
         body: JSON.stringify(dados)
         };
-        const url = "http://localhost:8080/categorias/gravar"
+        const url = window.servidor + '/categorias/'
         
         fetch(url, requestOptions)
             .then(console.log('gravado'))
@@ -37,38 +47,86 @@ export default class Categoria extends Component {
 
     render(){
         return(
-            <body >
-              <div className="container">
-                <h2>Lista de categorias</h2>
-                <div className="row">
-                    <div className="col ">ID</div>
-                    <div className="col">NOME</div>
-                    <form  >
-                        <input type="text" className="form"  placeholder="Buscar categoria"/>
-                    </form>
-                </div>
-                <div className="container-item">
-                    <div className="row mt-2">
-                        <div className="col-2">ID:</div>
-                        <div className="col-2">Nome:</div>
-                        <button type="button" className="btn btn-outline-primary ">ALTERAR</button>
-                        <button type="button" className="btn btn-outline-primary">EXCLUIR</button>
-                    </div>
-                
-                </div>
-              </div>
-              <div className="container-form ">
-                <h2>Cadastrar categorias</h2>
-                    <form  className="row">
-                        <input value={this.state.id} onChange={this.txtId_change} type="text" className="form" placeholder="id"/>
-                        <input value={this.state.nome} onChange={this.txtNome_change} type="text" className="form" placeholder="Nome"/>
-                        <button type="button" class="btn" onClick={() => this.gravarNovo()}>SALVAR</button>
-                        <button type="button" class="btn">DESFAZER</button>
-                    </form>
-                
-              </div>
+            <div className="container-fluid "> 
+                <div className="container  ">
+                    <h2 className="d-inline m-auto ">Lista de categorias</h2>
+                    {/*<div className="row justify-content-around ">
+                        <div className="col-sm">ID</div>
+                        <div className="col-sm">NOME</div>
+                        <form  >
+                            <input type="text" className="form-control-sm"  placeholder="Buscar categoria"/>
+                        </form>
+                    </div>*/}
+                    
+                    <br/>
+                    <table className="table bg-danger">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.categorias && this.state.categorias.map(categoria => {
+                                return <tr key={categoria.id}>
+                                    <th scope="row">{categoria.id}</th>
+                                    <td>{categoria.nome}</td>
+                                    </tr>
+                            })}
+                        </tbody>
+                    </table>
+                    
+                    <ul className="list-group ">
+                    {this.state.categorias && this.state.categorias.map(categoria => {
+                            return <li className="list-group-item " key={categoria.id}>
+            
+                                    <div className="col-sm-4 ml-5">{categoria.id}</div>
+                                    <div className="col-sm-2 mr-5">{categoria.nome}</div>
+                                    <button type="button" className="btn col p-2  btn-outline-primary ">ALTERAR</button>
+                                    <button type="button" className="btn col  p-2 btn-outline-primary">EXCLUIR</button>
+                                
+                            </li>
+                        })}
 
-            </body>
+                       {/* <button type="button" className="btn btn-primary pb-5 " data-toggle="modal" data-target="#exampleModal">Criar categoria</button>*/}
+                    </ul> 
+                     {/* MOdal */}
+                    <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h3 className="modal-title">Criar categoria</h3>
+                                    <button type="button" className="close" data-dismiss="modal">
+                                        <span>X</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <input type="text" className="form-control-sm "  placeholder="Nome da categoria"/>
+                                    
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn ml-3 btn-outline-primary" onClick={() => this.gravarNovo()}>SALVAR</button>
+                                </div>
+                                
+                            </div>
+                        
+                        </div>
+                    </div>
+                    {/* MOdal */}
+                
+                </div>
+                <div className="container-form">
+                    <h2>Cadastrar categorias</h2>
+                        <form  className="row">
+                        
+                            <input value={this.state.nome} onChange={this.txtNome_change} type="text" className="ml-3 form-control-sm p-0" placeholder="Nome"/>
+                            <button type="button" className="btn ml-3 btn-outline-primary" onClick={() => this.gravarNovo()}>SALVAR</button>
+                            <button type="button" className="btn ml-3 btn-outline-primary">DESFAZER</button>
+                        </form>
+                    
+                </div>
+                
+        </div>   
         )
     }
 }
