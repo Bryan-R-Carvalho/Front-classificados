3647a64e93b162daa7514b252a04dc21842ea8bd
Bryan-R-Carvalho <reisdecarvalhobryan@gmail.com>
2021-11-07 12:29:50 -0300

    atualizaçao de produtos

--- src/pages/produto/Produto.js
+++ src/pages/produto/Produto.js
@@ -100,101 +100,117 @@ export default class Produto extends Component {
                 console.log("deletado");
                 this.preencherLista();
             }).catch(erro => console.log(erro))
+            
     }
 
     renderExibirLista = () => {
         return(
-             
-            <div className="container-xl d-grid bg-white h-75 w-100 p-auto">
-                <h2 className="d-inline m-auto ">Lista de produtos</h2>            
-                <br/>
-                <table className="table ">
-                    <thead>
-                        <tr>
-                            <th className="col-sm-2" scope="col">Nome</th>
-                            <th className="col-sm-3" scope="col">Categoria</th>
-                            <th className="col-sm-2" scope="col">Disponibilidade</th>
-                            <th className="col-sm-3" scope="col">Aprovado</th>
-                            <th className="col-sm " scope="col">Alteraçoes</th>
-                        </tr>
-                    </thead>
-                </table>
-            
-                <ul className="list-group overflow-auto ">
-                {this.state.produtos && this.state.produtos.map(produto => {
-                        return <li className="list-group-item " key={produto.id}>
-                                <div className="row">
-                                    <div className="col-sm-2 ">{produto.nome}</div>
-                                    <div className="col-sm ">{produto.categoria.nome}</div>
-                                    <div className="col-sm ">{produto.disponibilidade}</div>
-                                    <div className="col-sm ">{produto.aprovado}</div>
-                                    <button type="button"  className="btn col-1 mx-3 btn-outline-primary btn-sm " onClick={() => this.iniciarAlterar(produto)}>ALTERAR</button>
-                                    <button type="button"  className="btn col-1 btn-outline-danger btn-sm" onClick={() => this.excluir(produto)}>EXCLUIR</button>
-                                </div>
-                        </li>
-                    })}
-                </ul> 
-                <button type="button"  className="btn col-2  btn-outline-primary btn-sm" onClick={() => this.iniciarNovo()}>Novo produto</button>
+            <div className="fundo container-fluid d-grid" >
+                <div className="container-xl d-grid bg-white h-75 w-100 p-auto">
+                    <h2 className="d-inline m-auto ">Lista de produtos</h2>            
+                    <br/>
+                    <table className="table ">
+                        <thead>
+                            <tr>
+                                <th className="col-sm-2" scope="col">Nome</th>
+                                <th className="col-sm-3" scope="col">Categoria</th>
+                                <th className="col-sm-2" scope="col">Disponibilidade</th>
+                                <th className="col-sm-3" scope="col">Aprovado</th>
+                                <th className="col-sm " scope="col">Alteraçoes</th>
+                            </tr>
+                        </thead>
+                    </table>
+                
+                    <ul className="list-group overflow-auto">
+                    {this.state.produtos && this.state.produtos.map(produto => {
+                            return <li className="list-group-item " key={produto.id}>
+                                    <div className="row">
+                                        <div className="col-sm-2 ">{produto.nome}</div>
+                                        <div className="col-sm ">{produto.categoria.nome}</div>
+                                        <div className="col-sm ">{produto.disponibilidade}</div>
+                                        <div className="col-sm ">{produto.aprovado}</div>
+                                        <div className="col-2 btn-group">
+                                            <button type="button" className="btn btn-outline-primary" onClick={() => this.iniciarAlterar(produto)}>Alterar</button>
+                                            <button type="button" className="btn btn-outline-danger" onClick={() => this.excluir(produto)}>Excluir</button>
+                                        </div>
+                                    </div>
+                            </li>
+                        })}
+                    </ul> 
+                    <button type="button"  className="btn col-2  btn-outline-primary btn-sm" onClick={() => this.iniciarNovo()}>Novo produto</button>
+                </div>
             </div>
-
         )
     }
     renderIncluir = () => {
         return(
-        <div className="container-xl d-grid bg-white p-auto">
-            <h2 className="m-auto">Cadastro de produtos</h2>
-            <form>
-                <div className="form-group pb-2 ">
-                    <label value={this.state.nome} type="text" className="col-sm-2">Nome do produto</label>
-                    <input className="form-control-sm mx-3 col-sm-3"  placeholder="Nome"/>
-                </div>
-                <div className="form-group  pb-2">
-                    <label className="col-sm-2" >Categoria</label>
-                        <select className="form-select-lg mx-3 col-sm-2 " onChange={this.cbocatChange} >
-                            {this.state.categorias.map((categoria) => (
-                                <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
-                         ))}
-                         
-                        </select>
-                </div>
+            <div className="fundo container-fluid d-grid" >
+                <div className="container-xl d-grid bg-white p-auto">
+                    <h2 className="m-auto">Cadastro de produtos</h2>
+                    <form>
+                        <div className="form-group pb-2 ">
+                            <label value={this.state.nome} type="text" className="col-sm-2">Nome do produto</label>
+                            <input className="form-control-sm mx-3 col-sm-3"  placeholder="Nome"/>
+                        </div>
+                        <div className="form-group  pb-2">
+                            <label className="col-sm-2" >Categoria</label>
+                                <select className="form-select-lg mx-3 col-sm-2 " onChange={this.cbocatChange} >
+                                    {this.state.categorias.map((categoria) => (
+                                        <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
+                                ))}
+                                
+                                </select>
+                        </div>
 
-                <div className="form-group pb-2">           
-                    <label className="pe-2">Aprovado</label>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="aprovacao"  value="true"/>
-                        <label className="form-check-label" >Sim</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="aprovacao" value="false"/>
-                        <label className="form-check-label" >Não</label>
-                    </div>
-                </div>
+                        <div className="form-group pb-2">           
+                            <label className="pe-2">Aprovado</label>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="aprovacao"  value="true"/>
+                                <label className="form-check-label" >Sim</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="aprovacao" value="false"/>
+                                <label className="form-check-label" >Não</label>
+                            </div>
+                        </div>
+                        
+                        <br/>
+                        <label className="pe-2">Disponibilidade</label>
+                        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" />
+                            <label className="btn btn-outline-primary" for="btnradio1">sim</label>
 
-                <div className="form-group pb-2">           
-                    <label className="pe-2">Disponibilidade</label>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="disponibilidade"  value="true"/>
-                        <label className="form-check-label" >Sim</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="disponibilidade"  value="false"/>
-                        <label className="form-check-label">Não</label>
-                    </div>
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" checked/>
+                            <label className="btn btn-outline-primary" for="btnradio2">Não</label>
+
+                        </div>
+
+                        <div className="form-group pb-2">           
+                            <label className="pe-2">Disponibilidade</label>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="disponibilidade"  value="true"/>
+                                <label className="form-check-label" >Sim</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="disponibilidade"  value="false"/>
+                                <label className="form-check-label">Não</label>
+                            </div>
+                        </div>
+                    
+                        <div className="form-group pb-2 ">
+                            <label className="col-sm-2" >Descrição</label>
+                            <textarea className="form-control-sm mx-3 pe-5 pb-2"  placeholder="Descreva o produto"/>
+                        </div>
+                        <div className="form-group pb-2 ">
+                            <label className="col-sm-2" >Justificativa</label>
+                            <textarea className="form-control-sm mx-3 pe-5 pb-2" />
+                        </div>
+                        <button type="submit" className="btn btn-primary px-5">Enviar</button>
+                        <button type="button"  className="btn col-2 btn-outline-primary btn-sm" onClick={() => this.iniciarExibir()}>Mostrar lista de produtos</button>
+                    </form>
+                    
                 </div>
-            
-                <div className="form-group pb-2 ">
-                    <label className="col-sm-2" >Descrição</label>
-                    <textarea className="form-control-sm mx-3 pe-5 pb-2"  placeholder="Descreva o produto"/>
-                </div>
-                <div className="form-group pb-2 ">
-                    <label className="col-sm-2" >Justificativa</label>
-                    <textarea className="form-control-sm mx-3 pe-5 pb-2" />
-                </div>
-                <button type="submit" className="btn btn-primary px-5">Enviar</button>
-                <button type="button"  className="btn col-2 btn-outline-primary btn-sm" onClick={() => this.iniciarExibir()}>Mostrar lista de produtos</button>
-            </form>
-            
-        </div>
+            </div>
         )
     }
     render() {
@@ -207,7 +223,7 @@ export default class Produto extends Component {
                 pagina = this.renderExibirLista()
             }else{
                 if(this.state.alterando){
-                    pagina = this.renderAlterar()
+                    pagina = this.renderIncluir()
                 }
             }
         }
