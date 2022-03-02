750a5c2e5d5eed2baa170a14b6e7a955e60d2019
Bryan R. Carvalho <62857493+Bryan-R-Carvalho@users.noreply.github.com>
2022-03-02 00:53:44 -0300

    main

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,6 @@ import Produto  from './pages/produto/Produto';
 import Cadastrar from './pages/cadastrar/CadastrarFornecedor';
 import Fornecedor from './pages/fornecedor/Fornecedor';
 import AprovarFornecedor from './pages/aprovar/Aprovar';
-<<<<<<< HEAD
-=======
-import Usuario from './pages/usuario/Usuario';
-import BuscarProduto from './pages/buscarProduto/BuscarProduto';
->>>>>>> main
 
 export default function Routes(){
       return(
@@ -21,11 +16,6 @@ export default function Routes(){
             <Route path="/cadastrar" component={Cadastrar}/>
             <Route path="/fornecedor" component={Fornecedor}/>
             <Route path="/aprovar" component={AprovarFornecedor}/>
-<<<<<<< HEAD
-=======
-            <Route path="/usuario" component={Usuario} />
-            <Route path="/buscarProduto" component={BuscarProduto} />
->>>>>>> main
           </Switch>
         </BrowserRouter>
       )
