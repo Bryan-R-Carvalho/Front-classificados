053b8a6eeb2e9ad2064c862edd590c0f7afb48bb
Bryan R. Carvalho <62857493+Bryan-R-Carvalho@users.noreply.github.com>
2022-03-02 00:33:42 -0300

    fix-produto

--- src/pages/produto/Produto.js
+++ src/pages/produto/Produto.js
@@ -108,8 +108,12 @@ export default class Produto extends Component {
                 'Content-Type': 'application/json',
             }
         };
+<<<<<<< Updated upstream
         //const url = window.servidor + "/produtos/" + produto.id
         const url = "http://localhost:8080" + "/produtos/" + produto.id
+=======
+        const url = window.servidor + "/produtos/" + produto.id
+>>>>>>> Stashed changes
         fetch(url, requestOptions)
             .then(resp => {
                 console.log("deletado");
