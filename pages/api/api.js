3b1e7731fa09bf0976a81c0247d9e1310af10f8b
Diogo Lanoir <diogolll@msn.com>
2022-06-13 16:54:35 -0300

    atualizado api

--- pages/api/api.js
+++ pages/api/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://classificados-back2.herokuapp.com",
+  //baseURL: "http://localhost:8080",
   headers: { "Content-Type": "application/json" },
 });
 
