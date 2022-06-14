b6d43bf6b9c2e452d3fabab4969a3adcdf3fa11f
Diogo Leonardo Lopes Lanoir <73046481+dllanoir@users.noreply.github.com>
2022-06-13 22:18:17 -0300

    Update api.js

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  daseURL: "https://classificados-back2.herokuapp.com",
+  baseURL: "https://classificados-back2.herokuapp.com",
 });
 
 api.defaults.headers.post["Content-Type"] = "application/json";
