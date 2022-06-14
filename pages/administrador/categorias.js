1714300e82fa8a509ff34d7c66e6cf85e9394c60
Diogo Lanoir <diogolll@msn.com>
2022-06-13 22:29:21 -0300

    ajustado categoria

--- pages/administrador/categorias.js
+++ pages/administrador/categorias.js
@@ -15,11 +15,12 @@ export default function Categorias({ categories }) {
   const onSubmit = async (data) => {
     try {
       const response = await api.post("/categorias/", JSON.stringify(data));
+      console.log(response);
       addToast({
         type: "success",
         title: "Categoria cadastrada com sucesso",
       });
-      setCategoriesList([...categoriesList, response]);
+      setCategoriesList([...categoriesList, response.data]);
       document.getElementById("form").reset();
     } catch (error) {
       addToast({
--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  aseURL: "https://classificados-back2.herokuapp.com",
+  baseURL: "https://classificados-back2.herokuapp.com",
 });
 
 api.defaults.headers.post["Content-Type"] = "application/json";
