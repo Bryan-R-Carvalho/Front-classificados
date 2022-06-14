b5f06e9b6123ae6e16685c9a3a9864e864dafd5b
Diogo Lanoir <diogolll@msn.com>
2022-06-13 21:07:36 -0300

    atualizado api

--- pages/api/api.js
+++ pages/api/api.js
@@ -4,7 +4,8 @@ const api = axios.create({
   //baseURL: "https://classificados-back2.herokuapp.com",
   baseURL: "https://classificadosmacae.herokuapp.com",
   //baseURL: "http://localhost:8080",
-  headers: { "Content-Type": "application/json" },
 });
+
+api.defaults.headers.post["Content-Type"] = "application/json";
 
 export default api;
